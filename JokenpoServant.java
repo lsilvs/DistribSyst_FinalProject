5456f08b1d9b5d8d1f4b1f64c910343dce5ebc37
Lucas Silvestre <lukas.silvestre@gmail.com>
2014-04-05 15:44:37 +0100

    Implements Jokenpo servant

---
+++ JokenpoServant.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.*;
+import org.omg.CORBA.*;
+
+import Jokenpo.*;
+
+class JokenpoServant implements HandlerGameOperations {
+
+	public void registerCB (Jokenpo.ClientOps c, org.omg.CORBA.Any option) {
+		
+
+	}
+
+}
