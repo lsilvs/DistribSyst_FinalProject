32ca4bf6f7c1e3ddc9c06084f4d8d5a4002ea565
Lucas Silvestre <lukas.silvestre@gmail.com>
2014-04-08 14:28:04 +0100

    Implements interfaces in new files

---
+++ Corba/ClientOpsCommunicationImpl.java
@@ -0,0 +1,47 @@
+package Corba;
+
+import javax.swing.JTextPane;
+
+import jokenpoGUI.View;
+
+import org.omg.CORBA.Any;
+import org.omg.CORBA.AnyHolder;
+
+import AddressBook.AddressAccountDetails;
+import AddressBook.AddressAccountDetailsHelper;
+
+class ClientOpsCommunicationImpl implements Communication.CommunicationOpsOperations {
+	public void callBackCreateChat(int chatId, int senderId, org.omg.CORBA.Any message) {
+		Client.setChatId(chatId);
+
+		View view = Client.getView();
+			
+		view.setVisible(true);
+        Client.getInvite().dispose();
+        
+        AnyHolder anyAccount = new AnyHolder();
+        Client.getAddressBookRef().get(senderId, anyAccount);
+        AddressAccountDetails sender = AddressAccountDetailsHelper.extract(anyAccount.value); 		
+        
+		JTextPane jTextPane1 = view.getJTextPane1();
+        jTextPane1.setText(sender.name + " invited you to a game");
+			
+		System.out.println("Message via callBack from server is " + message.extract_string());
+	}
+
+	public void callBackShowdMessage(int chatId, int senderId, Any message) {
+		View view = Client.getView();
+		
+		AnyHolder addressAccountDetailsOut = new AnyHolder();
+		Client.getAddressBookRef().get(senderId, addressAccountDetailsOut);
+		AddressAccountDetails accountDetails = AddressAccountDetailsHelper.extract(addressAccountDetailsOut.value);
+
+		JTextPane jTextPane1 = view.getJTextPane1();
+        String oldMessage = jTextPane1.getText() + "\n";
+        jTextPane1.setText(oldMessage + 
+        					accountDetails.name + ": " + 
+			        		message.extract_string());
+			
+		System.out.println("Message via callBack from server is " + message.extract_string());
+	}
+}
---
+++ Corba/ClientOpsJokenpoImpl.java
@@ -0,0 +1,41 @@
+package Corba;
+
+import javax.swing.JTextPane;
+
+import jokenpoGUI.View;
+
+import org.omg.CORBA.Any;
+import org.omg.CORBA.AnyHolder;
+
+import AddressBook.AddressAccountDetails;
+import AddressBook.AddressAccountDetailsHelper;
+
+class ClientOpsJokenpoImpl implements Jokenpo.JokenpoOpsOperations {
+	@Override
+	public void callBackCreateGame(int gameId, int senderId, Any message) {
+		Client.setGameId(gameId);
+	}
+
+	@Override
+	public void callBackShowdResult(int player1, int player2, Any message) {
+		View view = Client.getView();
+		
+		AnyHolder addressAccountDetailsOut = new AnyHolder();
+		Client.getAddressBookRef().get(player1, addressAccountDetailsOut);
+		AddressAccountDetails accountDetails1 = AddressAccountDetailsHelper.extract(addressAccountDetailsOut.value);
+		
+		Client.getAddressBookRef().get(player2, addressAccountDetailsOut);
+		AddressAccountDetails accountDetails2 = AddressAccountDetailsHelper.extract(addressAccountDetailsOut.value);
+
+		String result = message.extract_string().replace("$user1", accountDetails1.name)
+												.replace("$user2", accountDetails2.name);
+		
+		
+		JTextPane jTextPane1 = view.getJTextPane1();
+        String oldMessage = jTextPane1.getText() + "\n";
+        jTextPane1.setText(oldMessage + result);
+			
+		System.out.println("Message via callBack from server is " + message.extract_string());
+		
+	}
+}
